b0d27713fbff7266dc5dd57dd789bf69899bd4aa
ryu371 <longzhijiedaojian@gmail.com>
2026-03-10 09:27:40 +0900

    Add setup.py for package configuration

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="my_package",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        # 依存するライブラリを記述
+        "requests",
+    ],
+    author="ryu371",
+    description="A brief description",
+)
